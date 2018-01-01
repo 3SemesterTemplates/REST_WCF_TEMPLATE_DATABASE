d457216943cfb4d9884f2a0b2b0a383c6e777380
natsachaj <natascha_jakobsen@hotmail.com>
2018-01-01 16:13:48 +0100

    Add project files.

---
+++ REST_WCF_TEMPLATE_DATABASE/IService1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace REST_WCF_TEMPLATE_DATABASE
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        //HTTP
+        //Get all
+        [OperationContract]
+        [WebInvoke(
+                Method = "GET",
+                ResponseFormat = WebMessageFormat.Json,
+                UriTemplate = "movies")
+        ]
+        List<Movies> GetMovies();
+
+
+        //Get by id
+        [WebInvoke(
+                Method = "GET",
+                ResponseFormat = WebMessageFormat.Json,
+                UriTemplate = "movie/{id}")
+        ]
+        Movies GetOneMovie(String id);
+
+        //Post/create
+        [WebInvoke(
+                Method = "POST",
+                RequestFormat = WebMessageFormat.Json,
+                //ResponseFormat = WebMessageFormat.Json,
+                UriTemplate = "movies")
+        ]
+        void AddMovie(Movies newMovie);
+
+        //Delete
+        [WebInvoke(
+                Method = "DELETE",
+                ResponseFormat = WebMessageFormat.Json,
+                UriTemplate = "movies?id={id}")
+        ]
+        Movies DeleteMovie(int id);
+
+        //Update/Put
+        [WebInvoke(
+               Method = "PUT",
+               RequestFormat = WebMessageFormat.Json,
+               ResponseFormat = WebMessageFormat.Json,
+               UriTemplate = "movies")
+       ]
+        Movies UpdateMovie(Movies myMovie);
+
+
+
+      
+    }
+
+
+    
+}
---
+++ REST_WCF_TEMPLATE_DATABASE/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace REST_WCF_TEMPLATE_DATABASE
+{
+    [DataContract]
+    public class Movie
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Titel { get; set; }
+
+        [DataMember]
+        public string Rating { get; set; }
+
+
+
+        public Movie()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Titel: {Titel}, Rating: {Rating}";
+        }
+    }
+}
